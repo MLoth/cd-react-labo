d3e1064d3fced85b1058d6d0bf4e483660d042de
Martijn Loth <MLoth@users.noreply.github.com>
2022-03-09 14:00:42 +0100

    feat: basic layout added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import AppContainer from './components/AppContainer'
+import AppRow from './components/AppRow'
+import TodoInput from './components/TodoInput'
+
 function App() {
-  return <p>Lets go</p>
+  return (
+    <main>
+      {/* CONTAINER: Zet het in het midden */}
+      <AppContainer>
+        {/* ROW: Geeft padding tot de randen */}
+        <AppRow>
+          {/* Content */}
+          <TodoInput />
+        </AppRow>
+      </AppContainer>
+    </main>
+  )
 }
 
 export default App
